316dbfcebe59080eaa06bc3819ca201a5a89eded
tpircsboy <churehill@163.com>
2016-07-23 14:28:01 +0800

    Customize
    Customize original script for normal use

--- fswatch-rsync.sh
+++ fswatch-rsync.sh
@@ -23,10 +23,6 @@ FSWATCH_PATH="fswatch"
 # Sync latency / speed in seconds
 LATENCY="3"
 
-# default server setup
-MIDDLE="brute.aalto.fi" # middle ssh server
-TARGET="login2.triton.aalto.fi" # target ssh server
-
 # check color support
 colors=$(tput colors)
 if (($colors >= 8)); then
@@ -40,50 +36,60 @@ else
 fi
 
 # Check compulsory arguments
-if [[ "$1" = "" || "$2" = "" || "$3" = "" ]]; then
-  echo -e "${red}Error: $PROJECT takes 3 compulsory arguments.${nocolor}"
-  echo -n "Usage: fswatch-rsync.sh /local/path /targetserver/path ssh_user "
-  echo    "[middleserver] [targetserver] [target_ssh_user]"
+if [[ "$1" = "" || "$2" = "" ]]; then
+  echo -e "${red}Error: $PROJECT takes 2 compulsory arguments.${nocolor}"
+  echo "Usage: fswatch-rsync.sh src dest [ssh_port]"
+  echo "Dest might be local path, or remote ssh path like [USER@]HOST:DEST"
+  echo "Use ssh_port to specify ssh port"
   exit
 else
   LOCAL_PATH="$1"
   TARGET_PATH="$2"
-  SSH_USER="$3"
 fi
 
 # Check optional arguments
-if [[ "$4" != "" ]]; then
-  MIDDLE="$4"
-fi
-if [[ "$5" != "" ]]; then
-  TARGET="$5"
-fi
-if [[ "$6" != "" ]]; then
-  TARGET_SSH_USER="$6"
+if [[ "$3" != "" ]]; then
+  PORT="$3"
 else
-  TARGET_SSH_USER="$SSH_USER"
+  PORT="22"
 fi
+
 
 # Welcome
 echo      ""
 echo -e   "${green}Hei! This is $PROJECT v$VERSION.${nocolor}"
 echo      "Local source path:  \"$LOCAL_PATH\""
 echo      "Remote target path: \"$TARGET_PATH\""
-echo      "Via middle server:  \"$SSH_USER@$MIDDLE\""
-echo      "To target server:   \"$TARGET_SSH_USER@$TARGET\""
+echo      "Using ssh port:   \"$PORT\""
 echo      ""
 echo -n   "Performing initial complete synchronization "
-echo -n   "(Warning: Target directory will be overwritten "
-echo      "with local version if differences occur)."
+echo -n   "(Warning: one directory will be overwritten "
+echo      "with another version if differences occur)."
 
 # Perform initial complete sync
-read -n1 -r -p "Press any key to continue (or abort with Ctrl-C)... " key
+read -r -p "Please choose inital synchronization direction, up or down? [U/d]: " key
 echo      ""
-echo -n   "Synchronizing... "
-rsync -avzr -q --delete --force --exclude=".*" \
--e "ssh $SSH_USER@$MIDDLE ssh" $LOCAL_PATH $TARGET_SSH_USER@$TARGET:$TARGET_PATH
-echo      "done."
-echo      ""
+case "$key" in
+  "u"|"U"|"up"|"")
+    echo -n   "Synchronizing up... "
+    rsync -avzr -q --delete --force --exclude=".*" \
+    -e "ssh -p $PORT" $LOCAL_PATH $TARGET_PATH
+    echo      "done."
+    echo      ""
+    ;;
+  "d"|"D"|"down")
+    echo -n   "Synchronizing down... "
+    rsync -avzr -q --delete --force --exclude=".*" \
+    -e "ssh -p $PORT" $TARGET_PATH $LOCAL_PATH
+    echo      "done."
+    echo      ""
+    ;;
+  *)
+    echo "Unknown choice. Exiting..."
+    exit 1
+    ;;
+esac
+
 
 # Watch for changes and sync (exclude hidden files)
 echo    "Watching for changes. Quit anytime with Ctrl-C."
@@ -94,8 +100,8 @@ ${FSWATCH_PATH} -0 -r -l $LATENCY $LOCAL_PATH --exclude="/\.[^/]*$" \
     echo -en "${green}" `date` "${nocolor}\"$event\" changed. Synchronizing... "
     rsync -avzr -q --delete --force \
     --include-from=.tmp_files \
-    -e "ssh $SSH_USER@$MIDDLE ssh" \
-    $LOCAL_PATH $TARGET_SSH_USER@$TARGET:$TARGET_PATH
+    -e "ssh -p $PORT" \
+    $LOCAL_PATH $TARGET_PATH
   echo "done."
     rm -rf .tmp_files
   done
